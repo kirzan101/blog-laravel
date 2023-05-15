69ce9acf74d6b61e37deb488ea02dd2da59fc862
Apple Mae <cambeapplmae20@gmail.com>
2023-05-15 17:21:24 +0800

    added to suppliers

---
+++ app/Http/Resources/SupplierResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SupplierResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => (int) $this->getKey(),
+            'name' => $this->name,
+            'address' => $this->address,
+            'contact_number' => $this->contact_number,
+            'created_at' => $this->created_at,
+            'updated_by' => $this->updated_at,
+           //'comments' => $this->comments//CommentResource::collection($this->comments),
+    ];
+    }
+}
---
+++ app/Models/supplier.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class supplier extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'contact_number'
+    ];
+        /**
+     * get the list of comments associated to Post
+     *
+     * @return collections
+     */
+    public function item()
+    {
+        return $this->hasMany(Item::class);
+    }
+
+
+}
