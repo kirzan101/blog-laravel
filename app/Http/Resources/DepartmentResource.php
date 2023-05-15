b85588f6628c3a27940b58107890c3ddd610a91d
cardo <angelov.cabreros@gmail.com>
2023-05-14 22:53:36 -0700

    heto yung sa pag add na

---
+++ app/Http/Resources/DepartmentResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DepartmentResource extends JsonResource
+    {
+        /**
+         * Transform the resource into an array.
+         *
+         * @return array<string, mixed>
+         */
+        public function toArray(Request $request): array
+        {
+            return [
+                'id' => (int) $this->getKey(),
+                'name' => $this->name,
+                'code' => $this->code,
+                'contact_number' => $this->contact_number,
+                'description' => $this->description,
+                //'comments' => $this->comments//CommentResource::collection($this->comments),
+            ];
+        }
+    }
+
