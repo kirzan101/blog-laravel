687259d6bc98539e478cb2666835aef92164efc0
KrizzaG <krizzageralyn.diga1218212000@gmail.com>
2023-05-16 16:42:22 +0800

    updated accountability

--- app/Http/Controllers/AccountabilityController.php
+++ app/Http/Controllers/AccountabilityController.php
@@ -66,8 +66,7 @@ class AccountabilityController extends Controller
                 'item_id' => $request->item_id,
                 'department_id' => $request->department_id,
                 'status' => $request->status,
-                'updated_at' => $request->updated_at,
-                'created_at' => $request->created_at,
+                
             ]);
 
             return new AccountabilityResource($accountability);
