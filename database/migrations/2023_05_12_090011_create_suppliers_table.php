27f1b2dc40bcbc60c02749701b60e976a33cc190
Apple Mae <cambeapplmae20@gmail.com>
2023-05-16 15:06:13 +0800

    updated to supplier

--- database/migrations/2023_05_12_090011_create_suppliers_table.php
+++ database/migrations/2023_05_12_090011_create_suppliers_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('address');
-            $table->integer('contact_number');
+            $table->string('contact_number');
             $table->timestamps();
         });
     }
