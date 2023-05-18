5a0f4d3ff553be134196fc2b3817436b38224920
laurence <cambelaurence822@gmail.com>
2023-05-18 18:25:29 +0800

    updated again the controller

--- app/Http/Controllers/UserGroupController.php
+++ app/Http/Controllers/UserGroupController.php
@@ -16,11 +16,11 @@ class UserGroupController extends Controller
     public function index()
     {
         //all record
-        $usergroups = UserGroup::all();//select * from usergroup;
-        
+        $usergroups = UserGroup::all(); //select * from usergroup;
+
         // return $posts;
         return UserGroupResource::collection($usergroups); // for 2 or more records
-           }
+    }
 
     /**
      * Store a newly created resource in storage.
@@ -54,21 +54,19 @@ class UserGroupController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UserGroupRequest $request, UserGroup $usergroup)
+    public function update(UserGroupFormRequest $request, UserGroup $usergroup)
     {
-        try
-        {
+        try {
             //$usergroup = UserGroup::find($id);
 
             $usergroup = tap($usergroup)->update([
-                'name' => $request -> name,
-                'code' => $request -> code,
+                'name' => $request->name,
+                'code' => $request->code,
                 'description' => $request->description
             ]);
 
             return new UserGroupResource($usergroup);
-        } catch(\Exception $e)
-        {
+        } catch (\Exception $e) {
             return ['error' => 'has error - ' . $e];
         }
     }
