7ec1764182e50d34f9735ddfcceef7f82aeead1d
superman <bertlyncastillo55@gmail.com>
2023-05-19 15:23:23 +0800

    ulitin ulit

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
-
     protected $fillable = [
         'description',
         'brand',
@@ -16,17 +15,17 @@ class Item extends Model
         'department_id',
         'supplier_id',
     ];
-
+    //*this function belongs to supplier
     public function supplier()
     {
         return $this->belongsTo(Supplier::class);
     }
-
+    //*this function belongs to department
     public function department()
     {
         return $this->belongsTo(Department::class);
     }
-    
+    //*this function belongs to stocks
     public function stocks()
     {
         return $this->hasMany(Stock::class);
