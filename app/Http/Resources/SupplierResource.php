e907fc089ed3c7adeebfa6e4c479801163e70efc
christian <ckimescamilla@gmail.com>
2023-05-19 15:09:19 +0800

    fix fix stock unable to find & adjust item resource disply

--- app/Http/Resources/SupplierResource.php
+++ app/Http/Resources/SupplierResource.php
@@ -21,7 +21,6 @@ class SupplierResource extends JsonResource
             'contact_number' => $this->contact_number,
             'created_at' => $this->created_at,
             'updated_by' => $this->updated_at,
-            'items' => ItemResource::collection($this->items)
         ];
     }
 }
