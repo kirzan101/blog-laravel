5cdb84b32cfde314db1382b28db99643be9463d5
superman <bertlyncastillo55@gmail.com>
2023-05-19 10:48:48 +0800

    na edit na lahat

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -9,15 +9,10 @@ use Illuminate\Database\Seeder;
 
 class ItemSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         $department = Department::all()->first();
         $supplier = Supplier::all()->first();
-        // dd($department->getKey(), $supplier->getKey());
-
         $arrays = [
             [
                 'description' => 'With CORE i3',
