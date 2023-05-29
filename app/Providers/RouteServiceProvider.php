6175f49b39bb23cc571e5eebec11ad374c442220
christian <ckimescamilla@gmail.com>
2023-05-29 18:12:25 +0800

    added authentication

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -20,6 +20,15 @@ class RouteServiceProvider extends ServiceProvider
     public const HOME = '/home';
 
     /**
+     * The path to your application's "home" route.
+     *
+     * Typically, users are redirected here after authentication.
+     *
+     * @var string
+     */
+    public const INDEX = '/';
+
+    /**
      * Define your route model bindings, pattern filters, and other route configuration.
      */
     public function boot(): void
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -3,7 +3,7 @@
         @if (env('APP_NAME') != null)
             {{ env('APP_NAME') }}
         @else
-            Laravel
+            InventorySystem
         @endif
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
@@ -11,43 +11,46 @@
         <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item {{ Request::is('employees') ? 'active' : '' }}">
-                <a class="nav-link" href="/employees">Employees <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item {{ Request::is('departments') ? 'active' : '' }}">
-                <a class="nav-link" href="/departments">Departments <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item {{ Request::is('usergroups') ? 'active' : '' }}">
-                <a class="nav-link" href="/usergroups">User Groups <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item {{ Request::is('accountabilities') ? 'active' : '' }}">
-                <a class="nav-link" href="/accountabilities">Accountabilities <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item {{ Request::is('items') ? 'active' : '' }}">
-                <a class="nav-link" href="/items">Items <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item {{ Request::is('suppliers') ? 'active' : '' }}">
-                <a class="nav-link" href="/suppliers">Suppliers <span class="sr-only">(current)</span></a>
-            </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-            {{-- <p class="m-2">Test</p>
-            <button class="btn btn-danger my-2 my-sm-0" type="submit">Log out</button> --}}
+    @auth
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Hi! User
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Update password</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="/logout">Logout</a>
-                    </div>
+                <li class="nav-item {{ Request::is('employees') ? 'active' : '' }}">
+                    <a class="nav-link" href="/employees">Employees <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item {{ Request::is('departments') ? 'active' : '' }}">
+                    <a class="nav-link" href="/departments">Departments <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item {{ Request::is('usergroups') ? 'active' : '' }}">
+                    <a class="nav-link" href="/usergroups">User Groups <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item {{ Request::is('accountabilities') ? 'active' : '' }}">
+                    <a class="nav-link" href="/accountabilities">Accountabilities <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item {{ Request::is('items') ? 'active' : '' }}">
+                    <a class="nav-link" href="/items">Items <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item {{ Request::is('suppliers') ? 'active' : '' }}">
+                    <a class="nav-link" href="/suppliers">Suppliers <span class="sr-only">(current)</span></a>
                 </li>
             </ul>
-        </form>
-    </div>
+            <div class="form-inline my-2 my-lg-0">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Hi! {{ Auth::user()->username }}
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Update password</a>
+                            <div class="dropdown-divider"></div>
+                            <button class="dropdown-item" form="logout" type="submit">Logout</button>
+                            <form action="/logout" method="post" id="logout">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    @endauth
 </nav>
